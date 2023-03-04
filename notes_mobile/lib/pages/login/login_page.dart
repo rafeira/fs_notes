f2ee2d0987960815f3945a047a0351db1888e159
rafeira <rafael12wq@gmail.com>
2023-03-04 13:33:44 -0300

    Implement login page with login text field

--- notes_mobile/lib/pages/login/login_page.dart
+++ notes_mobile/lib/pages/login/login_page.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:notes_mobile/globals/widgets/default_scaffold.dart';
+import 'package:notes_mobile/pages/login/controllers/login_page_controller.dart';
+import 'package:notes_mobile/pages/login/local_widgets/auth_text_field.dart';
+import 'package:notes_mobile/pages/login/local_widgets/auth_title.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
+  LoginPage({super.key});
+  final controller = LoginPageController();
+  final divider = const SizedBox(
+    height: 30,
+  );
   @override
   Widget build(BuildContext context) {
-    return const DefaultScaffold(
-        body: Center(
-      child: Text(
-        'Login page',
-        style: TextStyle(color: Colors.white ),
+    return DefaultScaffold(
+      bodyPadding: const EdgeInsets.all(15.0),
+      body: Form(
+        key: controller.loginFormKey,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const AuthTitle(
+            title: 'Login',
+          ),
+          divider,
+          AuthTextField(
+            hintText: 'email@email.com',
+            suffix: const Icon(
+              Icons.alternate_email_rounded,
+              color: Colors.blue,
+            ),
+            label: 'Email',
+            textInputType: TextInputType.emailAddress,
+            validator: controller.emailValidator,
+            onFieldSubmitted: (_) async =>
+                await controller.onLoginButtonPressed(),
+            controller: controller.emailTextEditingController,
+          ),
+        ]),
       ),
-    ));
+    );
   }
 }
