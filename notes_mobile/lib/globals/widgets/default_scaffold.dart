3ba2b6b37e4c60b23e157090d431a121fa20d600
rafeira <rafael12wq@gmail.com>
2023-03-04 13:11:21 -0300

    Wrap body with safe are widget and add optional padding

--- notes_mobile/lib/globals/widgets/default_scaffold.dart
+++ notes_mobile/lib/globals/widgets/default_scaffold.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 class DefaultScaffold extends StatelessWidget {
-  const DefaultScaffold({super.key, required this.body});
+  const DefaultScaffold({super.key, required this.body, this.bodyPadding});
   final Widget body;
+  final EdgeInsetsGeometry? bodyPadding;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: body),
+      body: SafeArea(
+          child: Padding(
+        padding: bodyPadding ?? const EdgeInsets.all(8.0),
+        child: body,
+      )),
       backgroundColor: const Color.fromARGB(255, 55, 55, 55),
     );
   }
