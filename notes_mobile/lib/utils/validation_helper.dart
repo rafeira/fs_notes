443e890fc93946d1242f005965351cb8f1e39726
rafeira <rafael12wq@gmail.com>
2023-03-04 13:08:20 -0300

    Create validation helper class

---
+++ notes_mobile/lib/utils/validation_helper.dart
@@ -0,0 +1,7 @@
+class ValidationHelper {
+  static bool isEmailValid(String value) {
+    return RegExp(
+            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+        .hasMatch(value);
+  }
+}
