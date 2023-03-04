0a5eff7c9e4f1d838b53b68930c2b57f2766b929
rafeira <rafael12wq@gmail.com>
2023-03-04 13:09:41 -0300

    Create AuthTextField widget

---
+++ notes_mobile/lib/pages/login/local_widgets/auth_text_field.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AuthTextField extends StatelessWidget {
+  const AuthTextField(
+      {super.key,
+      this.hintText,
+      required this.controller,
+      this.suffix,
+      this.textInputType,
+      this.label, this.validator});
+  final String? hintText;
+  final TextEditingController controller;
+  final Widget? suffix;
+  final TextInputType? textInputType;
+  final String? label;
+  final FormFieldValidator<String>? validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: textInputType,
+      validator: validator,
+      decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+          suffix: suffix,
+          hintText: hintText,
+          label: label == null
+              ? null
+              : Text(
+                  label!,
+                ),
+          labelStyle: TextStyle(
+              fontWeight: FontWeight.w700,
+              background: Paint()..color = Colors.white),
+          alignLabelWithHint: true,
+          border: const OutlineInputBorder(),
+          fillColor: Colors.white,
+          filled: true),
+    );
+  }
+}
