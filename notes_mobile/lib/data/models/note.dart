1aa66b73ffee01ee0b3a70e37d383e7c3e8656b5
rafeira <rafael12wq@gmail.com>
2023-03-05 11:40:39 -0300

    Create note model and adapter

---
+++ notes_mobile/lib/data/models/note.dart
@@ -0,0 +1,20 @@
+import 'package:hive/hive.dart';
+part 'note.g.dart';
+@HiveType(typeId: 0)
+class Note extends HiveObject {
+  @HiveField(0)
+  int? id;
+  @HiveField(1)
+  String title;
+  @HiveField(2)
+  String content;
+
+  Note({this.id, required this.title, required this.content});
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'title': title, 'content': content};
+  }
+
+  factory Note.fromJson(Map<String, dynamic> json) =>
+      Note(id: json['id'], title: json['title'], content: json['content']);
+}
