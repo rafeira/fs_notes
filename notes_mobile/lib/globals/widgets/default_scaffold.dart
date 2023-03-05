aebd80c27f1cf6744dcbe11a72200257b4d5edf6
rafeira <rafael12wq@gmail.com>
2023-03-04 23:53:23 -0300

    Enable pass background color through arguments

--- notes_mobile/lib/globals/widgets/default_scaffold.dart
+++ notes_mobile/lib/globals/widgets/default_scaffold.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 class DefaultScaffold extends StatelessWidget {
-  const DefaultScaffold({super.key, required this.body, this.bodyPadding});
+  const DefaultScaffold(
+      {super.key,
+      required this.body,
+      this.bodyPadding,
+      this.backgroundColor = const Color.fromARGB(255, 55, 55, 55)});
   final Widget body;
   final EdgeInsetsGeometry? bodyPadding;
+  final Color backgroundColor;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,7 +17,7 @@ class DefaultScaffold extends StatelessWidget {
         padding: bodyPadding ?? const EdgeInsets.all(8.0),
         child: body,
       )),
-      backgroundColor: const Color.fromARGB(255, 55, 55, 55),
+      backgroundColor: backgroundColor,
     );
   }
 }
