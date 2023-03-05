dcddfbc283ef2353a96ffe95aacc9048823092ff
rafeira <rafael12wq@gmail.com>
2023-03-04 23:51:51 -0300

    Create credentials repository

---
+++ notes_mobile/lib/data/hive/credentials_repository.dart
@@ -0,0 +1,25 @@
+import 'package:notes_mobile/data/hive/hive_repository.dart';
+
+class CredentialsRepository extends HiveRepository<String> {
+  @override
+  String? get boxName => 'credentials';
+
+  final String emailKey = 'email';
+  final String passwordKey = 'username';
+
+  String? getUsername() {
+    return box!.get(emailKey);
+  }
+
+  String? getPassword() {
+    return box!.get(passwordKey);
+  }
+
+  Future<void> setUsername(String value) async {
+    return await box!.put(emailKey, value);
+  }
+
+  Future<void> setPassword(String value) async {
+    return await box!.put(passwordKey, value);
+  }
+}
