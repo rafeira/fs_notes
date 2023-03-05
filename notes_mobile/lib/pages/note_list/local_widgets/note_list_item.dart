9eb344e174ab98da71ffbadc4e88f427ee8b2b1d
rafeira <rafael12wq@gmail.com>
2023-03-05 00:05:46 -0300

    Create NoteListItem widget

---
+++ notes_mobile/lib/pages/note_list/local_widgets/note_list_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoteListItem extends StatelessWidget {
+  const NoteListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      child: SizedBox(
+        width: double.infinity,
+        height: 60.0,
+      ),
+    );
+  }
+}
