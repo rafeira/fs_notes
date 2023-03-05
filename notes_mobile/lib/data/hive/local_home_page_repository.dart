817e0ed64f9a5d5509586ed45ec9754f9d99ed37
rafeira <rafael12wq@gmail.com>
2023-03-04 23:10:50 -0300

    Create LocalHomePageRepository class

---
+++ notes_mobile/lib/data/hive/local_home_page_repository.dart
@@ -0,0 +1,11 @@
+import 'package:notes_mobile/data/hive/hive_repository.dart';
+import 'package:notes_mobile/routes/main/main_paths.dart';
+
+class LocalHomePageRepository extends HiveRepository<String> {
+  @override
+  String? get boxName => 'home_page';
+  final String key = 'key';
+  String getHome() {
+    return box!.get(key) ?? MainPaths.login;
+  }
+}
