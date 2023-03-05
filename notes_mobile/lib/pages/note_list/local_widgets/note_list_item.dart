fe05dab673c0dced254f8affce175704fb6c085f
rafeira <rafael12wq@gmail.com>
2023-03-05 11:41:31 -0300

    Use default card instead of card

--- notes_mobile/lib/pages/note_list/local_widgets/note_list_item.dart
+++ notes_mobile/lib/pages/note_list/local_widgets/note_list_item.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:notes_mobile/globals/widgets/default_card.dart';
 
 class NoteListItem extends StatelessWidget {
   const NoteListItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
-      child: SizedBox(
-        width: double.infinity,
-        height: 60.0,
-      ),
+    return const DefaultCard(
+      child: SizedBox(),
     );
   }
 }
