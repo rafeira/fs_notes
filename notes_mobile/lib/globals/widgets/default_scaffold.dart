10412e6b7f478c6bd5e054fcb49fea27d5084012
rafeira <rafael12wq@gmail.com>
2023-03-07 19:06:11 -0300

    Pass appBar widget through arguments

--- notes_mobile/lib/globals/widgets/default_scaffold.dart
+++ notes_mobile/lib/globals/widgets/default_scaffold.dart
@@ -5,13 +5,16 @@ class DefaultScaffold extends StatelessWidget {
       {super.key,
       required this.body,
       this.bodyPadding,
-      this.backgroundColor = const Color.fromARGB(255, 55, 55, 55)});
+      this.backgroundColor = const Color.fromARGB(255, 55, 55, 55),
+      this.appBar});
   final Widget body;
   final EdgeInsetsGeometry? bodyPadding;
   final Color backgroundColor;
+  final PreferredSizeWidget? appBar;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: appBar,
       body: SafeArea(
           child: Padding(
         padding: bodyPadding ?? const EdgeInsets.all(8.0),
--- notes_mobile/lib/pages/note_list/note_list_page.dart
+++ notes_mobile/lib/pages/note_list/note_list_page.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:notes_mobile/globals/widgets/default_app_bar.dart';
 import 'package:notes_mobile/globals/widgets/default_scaffold.dart';
+import 'package:notes_mobile/pages/note_list/local_widgets/note_list_item.dart';
 
 class NoteListPage extends StatelessWidget {
   const NoteListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const DefaultScaffold(
-      backgroundColor: Color.fromARGB(255, 160, 160, 160),
-      body: Center(
-        child: Text('NoteListPage'),
+    return DefaultScaffold(
+      appBar: const DefaultAppBar(),
+      backgroundColor: const Color.fromARGB(255, 160, 160, 160),
+      body: ListView.builder(
+        itemCount: 100,
+        itemBuilder: (context, index) => const NoteListItem(),
       ),
     );
   }
