fa66e52abf4f2522d8529505e40de214701b936c
rafeira <rafael12wq@gmail.com>
2023-03-07 19:05:08 -0300

    Create default app bar widget

---
+++ notes_mobile/lib/globals/widgets/default_app_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const DefaultAppBar(
+      {super.key, this.backgroundColor, this.elevation, this.actions});
+  final Color? backgroundColor;
+  final double? elevation;
+  final List<Widget>? actions;
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: backgroundColor ?? Colors.transparent,
+      elevation: elevation ?? 0,
+      actions: actions,
+    );
+  }
+}
