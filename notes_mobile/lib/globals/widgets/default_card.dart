b1a24173ae6bc3f520df0077ad16df294af47894
rafeira <rafael12wq@gmail.com>
2023-03-08 21:34:20 -0300

    Add card color field

--- notes_mobile/lib/globals/widgets/default_card.dart
+++ notes_mobile/lib/globals/widgets/default_card.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 
 class DefaultCard extends StatelessWidget {
   const DefaultCard(
-      {super.key, this.padding = const EdgeInsets.all(8.0), this.child});
+      {super.key,
+      this.padding = const EdgeInsets.all(8.0),
+      this.child,
+      this.cardColor});
   final EdgeInsetsGeometry padding;
   final Widget? child;
+  final Color? cardColor;
   @override
   Widget build(BuildContext context) {
     return Card(
+        color: cardColor,
         child: Padding(
-      padding: padding,
-      child: child,
-    ));
+          padding: padding,
+          child: child,
+        ));
   }
 }
