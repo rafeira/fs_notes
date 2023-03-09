a3ce2d4024460038e24ff184e1ec4ac41167e0a5
rafeira <rafael12wq@gmail.com>
2023-03-09 20:41:50 -0300

    Replace note with title and content fields

--- notes_mobile/lib/pages/note_list/local_widgets/note_list_item.dart
+++ notes_mobile/lib/pages/note_list/local_widgets/note_list_item.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:notes_mobile/globals/widgets/default_card.dart';
 
 class NoteListItem extends StatelessWidget {
-  const NoteListItem({super.key});
-
+  const NoteListItem({super.key, required this.title, required this.content});
+  final String title;
+  final String content;
   @override
   Widget build(BuildContext context) {
-    return const DefaultCard(
-      child: SizedBox(),
-    );
+    return DefaultCard(
+        cardColor: const Color.fromARGB(255, 30, 41, 30),
+        child: ListTile(
+          title: Text(title),
+          subtitle: Text(content),
+        ));
   }
 }
