9e46980239dc0c80d1c8f87c7140b30158eba6d5
rafeira <rafael12wq@gmail.com>
2023-03-08 21:36:43 -0300

    Implement note list page

--- notes_mobile/lib/pages/note_list/note_list_page.dart
+++ notes_mobile/lib/pages/note_list/note_list_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:notes_mobile/globals/widgets/default_app_bar.dart';
 import 'package:notes_mobile/globals/widgets/default_scaffold.dart';
+import 'package:notes_mobile/globals/widgets/default_title.dart';
+import 'package:notes_mobile/pages/login/local_widgets/default_button.dart';
 import 'package:notes_mobile/pages/note_list/local_widgets/note_list_item.dart';
 
 class NoteListPage extends StatelessWidget {
@@ -13,9 +15,40 @@ class NoteListPage extends StatelessWidget {
     return DefaultScaffold(
       appBar: const DefaultAppBar(),
       backgroundColor: const Color.fromARGB(255, 160, 160, 160),
-      body: ListView.builder(
-        itemCount: 100,
-        itemBuilder: (context, index) => const NoteListItem(),
+      body: Column(
+        children: [
+          const DefaultTitle(
+            text: 'Minhas anotações',
+            fontSize: 25,
+          ),
+          divider,
+          Align(
+            alignment: Alignment.centerLeft,
+            child: DefaultButton(
+              onTap: () => false,
+              title: 'Adicionar',
+            ),
+          ),
+          divider,
+          Flexible(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 10,
+              itemBuilder: (context, index) => Dismissible(
+                direction: DismissDirection.startToEnd,
+                key: Key(index.toString()),
+                child: NoteListItem(
+                  key: Key(index.toString()),
+                ),
+              ),
+            ),
+          ),
+          divider,
+          DefaultButton(
+            onTap: () => false,
+            title: '+',
+          )
+        ],
       ),
     );
   }
