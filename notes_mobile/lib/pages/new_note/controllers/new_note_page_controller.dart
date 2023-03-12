d6a66a1e8403ced5c5aa2a0b183f39c02018d014
rafeira <rafael12wq@gmail.com>
2023-03-12 17:17:49 -0300

    Add note controller

--- notes_mobile/lib/pages/new_note/controllers/new_note_page_controller.dart
+++ notes_mobile/lib/pages/new_note/controllers/new_note_page_controller.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:notes_mobile/data/models/note.dart';
 
 class NewNotePageController {
   final formKey = GlobalKey<FormState>();
   final titleEditingController = TextEditingController();
   final contentEditingController = TextEditingController();
+  Note? note;
 }
