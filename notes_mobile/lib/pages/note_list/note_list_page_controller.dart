3644333831c7def9b117a8ffce29a921b1228207
rafeira <rafael12wq@gmail.com>
2023-03-12 13:30:43 -0300

    Set buttons callbacks

--- notes_mobile/lib/pages/note_list/note_list_page_controller.dart
+++ notes_mobile/lib/pages/note_list/note_list_page_controller.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/widgets.dart';
 import 'package:notes_mobile/data/models/note.dart';
-import 'package:notes_mobile/routes/main/main_paths.dart';
+import 'package:notes_mobile/globals/pages/default%20_message/navigation_hepers/arguments/default_message_arguments.dart';
+import 'package:notes_mobile/globals/pages/default%20_message/navigation_hepers/default_message_navigator_helper.dart';
 
 class NoteListPageController {
   final noteList = <Note>[];
 
-  Future<void> navigate(BuildContext context) async {
-    await Navigator.pushNamed(context, MainPaths.newNote);
+  Future<void> onPlusButtonPressed(NavigatorState navigatorState) async {}
+
+  Future<void> onAddButtonPressed(NavigatorState navigatorState) async {}
+
+  Future<void> _navigateToNotLoggedMessage(
+      NavigatorState navigatorState) async {
+    final model = DefaultMessageArguments(
+        assetImage: '',
+        message: 'Ola mundo',
+        confirmButtonCallback: () => print('confirm'));
+    await DefaultMessageNavigatorHelper.navigate(
+        navigatorState: navigatorState, model: model);
   }
 
-  Future<void> onSyncButtonPressed(BuildContext context) async {
-    await Future.delayed(const Duration(seconds: 1));
-    await _navigateToLogin(context);
+  Future<void> onSyncButtonPressed(NavigatorState navigatorState) async {
+    if (await _isLoggedIn()) {
+    } else {
+      await _navigateToNotLoggedMessage(navigatorState);
+    }
+  }
+
+  Future<bool> _isLoggedIn() async {
+    return false;
   }
 }
