8553d305054976c46205779f33a48783b166dd7d
rafeira <rafael12wq@gmail.com>
2023-03-12 17:19:47 -0300

    Add controller and validator fields

--- notes_mobile/lib/pages/new_note/local_widges/note_form_field.dart
+++ notes_mobile/lib/pages/new_note/local_widges/note_form_field.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
 class NoteFormField extends StatelessWidget {
-  const NoteFormField({super.key, this.maxLines, this.label});
+  const NoteFormField(
+      {super.key, this.maxLines, this.label, this.controller, this.validator});
   final int? maxLines;
   final String? label;
-  final enabledBorder = const OutlineInputBorder(
-      borderSide:
-          BorderSide(width: .8, color: Color.fromARGB(255, 102, 100, 100)));
+  final TextEditingController? controller;
+  final FormFieldValidator<String>? validator;
+
   final focusedBorder = const OutlineInputBorder(
       borderSide:
           BorderSide(width: .8, color: Color.fromARGB(255, 233, 233, 233)));
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
       maxLines: maxLines,
+      validator: validator,
       cursorColor: const Color.fromARGB(255, 70, 70, 70),
       decoration: InputDecoration(
         fillColor: Colors.red,
