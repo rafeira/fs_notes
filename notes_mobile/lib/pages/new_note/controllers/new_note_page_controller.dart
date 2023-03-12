3c39297a4ada58da0fc124617d4b91aab967d5f3
rafeira <rafael12wq@gmail.com>
2023-03-12 17:17:28 -0300

    Add title and content editing controller

--- notes_mobile/lib/pages/new_note/controllers/new_note_page_controller.dart
+++ notes_mobile/lib/pages/new_note/controllers/new_note_page_controller.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class NewNotePageController {
   final formKey = GlobalKey<FormState>();
+  final titleEditingController = TextEditingController();
+  final contentEditingController = TextEditingController();
 }
