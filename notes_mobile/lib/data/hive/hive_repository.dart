dff912e2eef0190eadc03742239c40d3f16b9e8a
rafeira <rafael12wq@gmail.com>
2023-03-14 20:14:40 -0300

    Handle hive errors

--- notes_mobile/lib/data/hive/hive_repository.dart
+++ notes_mobile/lib/data/hive/hive_repository.dart
@@ -2,13 +2,29 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 abstract class HiveRepository<T> {
   String? boxName;
-  Box<T>? box;
+  Box<T>? _box;
+
+  Box<T> get box {
+    if (_box != null) {
+      return _box!;
+    } else {
+      throw 'Box not initialized';
+    }
+  }
 
   Future<void> openBox() async {
-    box = await Hive.openBox<T>(boxName!);
+    try {
+      _box = await Hive.openBox<T>(boxName!);
+    } on HiveError {
+      rethrow;
+    } 
   }
 
   Future<void> closeBox() async {
-    await box?.close();
+    await box.close();
+  }
+
+  bool isOpen() {
+    return _box?.isOpen ?? false;
   }
 }
