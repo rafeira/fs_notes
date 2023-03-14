21e5c3f7bafe9d861c8a88fa32814bdac6f38dc7
rafeira <rafael12wq@gmail.com>
2023-03-14 20:14:09 -0300

    Create local notes repository

---
+++ notes_mobile/lib/data/hive/notes/local_notes_repository.dart
@@ -0,0 +1,23 @@
+import 'package:hive/hive.dart';
+import 'package:notes_mobile/data/hive/hive_repository.dart';
+import 'package:notes_mobile/data/models/note.dart';
+
+class LocalNotesRepository extends HiveRepository<Note> {
+  @override
+  String? get boxName => 'notes';
+  Future<void> add(Note note) async {
+    try {
+      await box.add(note);
+    } on HiveError {
+      rethrow;
+    }
+  }
+
+  Future<List<Note>> getAllNotes() async {
+    try {
+      return box.values.toList();
+    } on HiveError {
+      rethrow;
+    }
+  }
+}
