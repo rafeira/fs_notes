badc3982ee7f55792f1bda6c372339e1d23f71e6
rafeira <rafael12wq@gmail.com>
2023-03-14 22:19:52 -0300

    Implement page

--- notes_mobile/lib/globals/pages/default_message/default_message_page.dart
+++ notes_mobile/lib/globals/pages/default_message/default_message_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notes_mobile/globals/pages/default_message/navigation_hepers/arguments/default_message_arguments.dart';
 import 'package:notes_mobile/globals/widgets/default_app_bar.dart';
 import 'package:notes_mobile/globals/widgets/default_scaffold.dart';
+import 'package:notes_mobile/pages/login/local_widgets/default_button.dart';
 
 class DefaultMessagePage extends StatelessWidget {
   const DefaultMessagePage({super.key});
@@ -21,8 +22,40 @@ class DefaultMessagePage extends StatelessWidget {
     final arguments = _getArguments(context);
     return DefaultScaffold(
         appBar: const DefaultAppBar(),
-        body: Column(
-          children: [Text(arguments.message)],
+        body: Align(
+          alignment: Alignment.topCenter,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              if (arguments.assetImage != null)
+                Image(
+                  image: AssetImage(arguments.assetImage!),
+                  width: 120.0,
+                  height: 120.0,
+                  color: Colors.white,
+                ),
+              Text(
+                arguments.message,
+                style: const TextStyle(fontSize: 25.0, color: Colors.white),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  DefaultButton(
+                    title: 'Voltar',
+                    onTap: arguments.declineButtonCallback,
+                    backgroundColor: Colors.blue,
+                    borderColor: const Color.fromARGB(255, 86, 172, 241),
+                  ),
+                  DefaultButton(
+                    title: 'Fazer login',
+                    onTap: arguments.confirmButtonCallback,
+                  )
+                ],
+              )
+            ],
+          ),
         ));
   }
 }
