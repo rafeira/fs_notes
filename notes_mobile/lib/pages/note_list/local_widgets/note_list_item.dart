c5fc7784008a564d3cb2fa1c37a4659c05bbdf79
rafeira <rafael12wq@gmail.com>
2023-03-15 21:00:44 -0300

    Customize note list item

--- notes_mobile/lib/pages/note_list/local_widgets/note_list_item.dart
+++ notes_mobile/lib/pages/note_list/local_widgets/note_list_item.dart
@@ -9,16 +9,28 @@ class NoteListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultCard(
         elevation: 10.0,
-        cardColor: const Color.fromARGB(255, 21, 114, 109),
-        child: ListTile(
-          title: Text(
-            "$title $key",
-            style: const TextStyle(color: Colors.white),
-          ),
-          subtitle: Text(
-            content,
-            style: const TextStyle(color: Color.fromARGB(255, 131, 131, 131)),
-          ),
+        cardColor: const Color.fromARGB(255, 93, 95, 95),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(bottom: 8.0),
+              child: Text(
+                title,
+                style: const TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+            DefaultCard(
+              elevation: 9.0,
+              margin: EdgeInsets.zero,
+              width: double.infinity,
+              cardColor: const Color.fromARGB(255, 154, 156, 156),
+              child: Text(
+                content,
+                style: const TextStyle(color: Color.fromARGB(255, 93, 95, 95)),
+              ),
+            ),
+          ],
         ));
   }
 }
