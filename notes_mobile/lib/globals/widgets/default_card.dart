384a12531ff9e1616cabc1aba7afec2df04d19ff
rafeira <rafael12wq@gmail.com>
2023-03-15 21:00:03 -0300

    Add card and width fields

--- notes_mobile/lib/globals/widgets/default_card.dart
+++ notes_mobile/lib/globals/widgets/default_card.dart
@@ -6,17 +6,23 @@ class DefaultCard extends StatelessWidget {
       this.padding = const EdgeInsets.all(8.0),
       this.child,
       this.cardColor,
-      this.elevation});
+      this.elevation,
+      this.margin,
+      this.width});
   final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry? margin;
   final Widget? child;
   final Color? cardColor;
   final double? elevation;
+  final double? width;
   @override
   Widget build(BuildContext context) {
     return Card(
         elevation: elevation,
+        margin: margin,
         color: cardColor,
-        child: Padding(
+        child: Container(
+          width: width,
           padding: padding,
           child: child,
         ));
