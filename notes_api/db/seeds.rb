726e524d0c04564eab321e023197f711b8d3f829
rafeira <rafael12wq@gmail.com>
2023-03-25 18:10:52 -0300

    Set notes seed

--- notes_api/db/seeds.rb
+++ notes_api/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+100.times do
+  Note.create(
+    title: Faker::Lorem.sentence(word_count: 5),
+    content: Faker::Lorem.paragraph(sentence_count: 90)
+  )
+end
