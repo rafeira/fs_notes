f728e3b26d08d72760cc8e18786f1a38db95196b
rafeira <rafael12wq@gmail.com>
2023-03-25 17:26:33 -0300

    Update schema

--- notes_api/db/schema.rb
+++ notes_api/db/schema.rb
@@ -11,10 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_25_201206) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
