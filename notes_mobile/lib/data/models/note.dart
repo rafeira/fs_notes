74bb5b2139c055868c840c714f051c6969b3ba3c
rafeira <rafael12wq@gmail.com>
2023-03-25 21:29:46 -0300

    Add timestamp fields

--- notes_mobile/lib/data/models/note.dart
+++ notes_mobile/lib/data/models/note.dart
@@ -9,13 +9,42 @@ class Note extends HiveObject {
   String title;
   @HiveField(2)
   String content;
+  @HiveField(3)
+  DateTime? createdAt;
+  @HiveField(4)
+  DateTime? updatedAt;
 
-  Note({this.id, required this.title, required this.content});
+  Note(
+      {this.id,
+      required this.title,
+      required this.content,
+      this.createdAt,
+      this.updatedAt});
 
   Map<String, dynamic> toJson() {
-    return {'id': id, 'title': title, 'content': content};
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'updated_at': updatedAt,
+      'created_at': createdAt
+    };
   }
 
-  factory Note.fromJson(Map<String, dynamic> json) =>
-      Note(id: json['id'], title: json['title'], content: json['content']);
+  factory Note.fromJson(Map<String, dynamic> json) => Note(
+      id: json['id'],
+      title: json['title'],
+      content: json['content'],
+      createdAt: _dateFromJson(json['created_at']),
+      updatedAt: _dateFromJson(json['updated_at']));
+
+  static DateTime? _dateFromJson(String? value) {
+    if (value != null) return DateTime.tryParse(value);
+    return null;
+  }
+
+  @override
+  String toString() {
+    return ' (id: $id, title: $title, createdAt: $createdAt) ';
+  }
 }
--- notes_mobile/lib/data/models/note.g.dart
+++ notes_mobile/lib/data/models/note.g.dart
@@ -20,19 +20,25 @@ class NoteAdapter extends TypeAdapter<Note> {
       id: fields[0] as int?,
       title: fields[1] as String,
       content: fields[2] as String,
+      createdAt: fields[3] as DateTime?,
+      updatedAt: fields[4] as DateTime?,
     );
   }
 
   @override
   void write(BinaryWriter writer, Note obj) {
     writer
-      ..writeByte(3)
+      ..writeByte(5)
       ..writeByte(0)
       ..write(obj.id)
       ..writeByte(1)
       ..write(obj.title)
       ..writeByte(2)
-      ..write(obj.content);
+      ..write(obj.content)
+      ..writeByte(3)
+      ..write(obj.createdAt)
+      ..writeByte(4)
+      ..write(obj.updatedAt);
   }
 
   @override
