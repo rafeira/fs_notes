b97402ae4e2f844908bcc1c396de2b8155cc9751
rafeira <rafael12wq@gmail.com>
2023-04-21 19:43:08 -0300

    Setup valid headers to every request

--- notes_api/app/controllers/application_controller.rb
+++ notes_api/app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :ensure_json_request
   include ActionController::MimeResponds
   respond_to :json
+
+
+  def ensure_json_request
+    return if /vnd.api\+json/.match?(request.headers['Accept'])
+
+    render json: '', status: :not_acceptable
+  end
 end
