1c6f2ca76027d235fcd4a954df6f73bfc8a5478f
rafeira <rafael12wq@gmail.com>
2023-04-21 19:42:27 -0300

    Implement create user sessions controller action

--- notes_api/app/controllers/api/v1/sessions/users_sessions_controller.rb
+++ notes_api/app/controllers/api/v1/sessions/users_sessions_controller.rb
@@ -1,5 +1,23 @@
-class UsersSessionsController < Devise::SessionsController
-  def create
-    super { |resource| @resource = resource }
+module Api
+  module V1
+    module Sessions
+      class UsersSessionsController < Devise::SessionsController
+        def create
+          user = User.find_by(email: user_params[:email])
+          if user && user.valid_password?(user_params[:password])
+            token = JWT.encode({user_id: user.id}, Rails.application.secret_key_base)
+            render json: { user: user, token: token }
+          else
+            render json: { error: 'Invalid email or password' }, status: :unprocessable_entity
+          end
+        end
+
+
+        private
+        def user_params
+          params.require(:user).permit(:email, :password)
+        end
+      end
+    end
   end
 end
