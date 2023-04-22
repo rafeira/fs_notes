9f1b82edc7f6f60714d3939f6b5689bf13f837f3
rafeira <rafael12wq@gmail.com>
2023-04-22 18:56:38 -0300

    Fix params

--- notes_api/app/controllers/api/v1/sessions/users_sessions_controller.rb
+++ notes_api/app/controllers/api/v1/sessions/users_sessions_controller.rb
@@ -3,8 +3,8 @@ module Api
     module Sessions
       class UsersSessionsController < Devise::SessionsController
         def create
-          user = User.find_by(email: user_params[:email])
-          if user && user.valid_password?(user_params[:password])
+          user = User.find_by(email: params[:email])
+          if user && user.valid_password?(params[:password])
             token = JWT.encode({user_id: user.id}, Rails.application.secret_key_base)
             set_authorization_header token
             render json: { user: user }
