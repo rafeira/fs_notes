a9de9fad352ff2303a5c3d62fe362dca72952864
rafeira <rafael12wq@gmail.com>
2023-04-22 18:56:57 -0300

    Set user model as jwt_authenticable

--- notes_api/app/models/user.rb
+++ notes_api/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :jwt_authenticatable,
          :recoverable, :rememberable, :validatable, jwt_revocation_strategy: JwtDenylist
 end
