bfbafc674b8554a59f341ebc4ce4b2cd014d9adb
rafeira <rafael12wq@gmail.com>
2023-04-22 18:56:24 -0300

    Do not return token in response but in Authorization header

--- notes_api/app/controllers/api/v1/sessions/users_sessions_controller.rb
+++ notes_api/app/controllers/api/v1/sessions/users_sessions_controller.rb
@@ -6,7 +6,8 @@ module Api
           user = User.find_by(email: user_params[:email])
           if user && user.valid_password?(user_params[:password])
             token = JWT.encode({user_id: user.id}, Rails.application.secret_key_base)
-            render json: { user: user, token: token }
+            set_authorization_header token
+            render json: { user: user }
           else
             render json: { error: 'Invalid email or password' }, status: :unauthorized
           end
