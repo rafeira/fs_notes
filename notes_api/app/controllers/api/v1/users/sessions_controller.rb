4bc4a327d1665026c44932f5b8c95bc855acd800
rafeira <rafael12wq@gmail.com>
2023-05-01 16:11:05 -0300

    Rename and refactor sessions controller

---
+++ notes_api/app/controllers/api/v1/users/sessions_controller.rb
@@ -0,0 +1,29 @@
+module Api
+  module V1
+    module Users
+      class SessionsController < Devise::SessionsController
+        respond_to :json
+
+        private
+    
+        def respond_with(_resource, _opts = {})
+          render json: { message: 'Login efetuado com sucesso!' }, status: :ok
+        end
+    
+        def respond_to_on_destroy
+          log_out_success && return if current_user
+    
+          log_out_failure
+        end
+    
+        def log_out_success
+          render json: { message: 'Saiu com sucesso.' }, status: :ok
+        end
+    
+        def log_out_failure
+          render json: { message: 'Hmm nada aconteceu.' }, status: :unauthorized
+        end
+      end
+    end
+  end
+end
