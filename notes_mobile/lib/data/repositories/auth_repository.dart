55844d5de2118ba2a4eef8e7df75bcd49ec67482
rafeira <rafael12wq@gmail.com>
2023-05-02 20:15:19 -0300

    Set token after get from api

--- notes_mobile/lib/data/repositories/auth_repository.dart
+++ notes_mobile/lib/data/repositories/auth_repository.dart
@@ -1,12 +1,16 @@
 import 'dart:convert';
 
+import 'package:notes_mobile/data/models/response_token.dart';
 import 'package:notes_mobile/data/providers/auth_provider.dart';
+import 'package:notes_mobile/data/secure/repositories/local_auth_repository.dart';
 
 class AuthRepository {
   final _provider = AuthProvider();
+  final _locaAuthRepository = LocalAuthRepository();
   Future<void> signIn({required String email, required String password}) async {
     final response = await _provider.signIn(email: email, password: password);
-    final jsonNotes = _decodedIterableBody(response.body);
+    final userMap = _decodedIterableBody(response.body);
+    await _locaAuthRepository.setToken(getTokenFromHeader(response.headers));
   }
 
   Iterable _decodedIterableBody(String body) {
