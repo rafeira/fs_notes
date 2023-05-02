720b4fda504cd4ac106b68646a72ab48d10f5a32
rafeira <rafael12wq@gmail.com>
2023-05-01 22:32:49 -0300

    Fix jwt denylist migration

--- notes_api/db/migrate/20230422182807_create_jwt_denylist.rb
+++ notes_api/db/migrate/20230422182807_create_jwt_denylist.rb
@@ -1,11 +1,9 @@
 class CreateJwtDenylist < ActiveRecord::Migration[7.0]
   def change
-    def change
-      create_table :jwt_denylist do |t|
-        t.string :jti, null: false
-        t.datetime :exp, null: false
-      end
-      add_index :jwt_denylist, :jti
+    create_table :jwt_denylist do |t|
+      t.string :jti, null: false
+      t.datetime :exp, null: false
     end
+    add_index :jwt_denylist, :jti
   end
 end
--- notes_api/db/schema.rb
+++ notes_api/db/schema.rb
@@ -14,6 +14,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_22_182807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
+
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
