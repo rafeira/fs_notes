3e19e7c4aeabf9daa3e9278fd0bf61619c7a382a
rafeira <rafael12wq@gmail.com>
2023-05-05 20:40:10 -0300

    Encode body before send it

--- notes_mobile/lib/data/providers/auth_provider.dart
+++ notes_mobile/lib/data/providers/auth_provider.dart
@@ -1,11 +1,15 @@
+import 'dart:convert';
+
 import 'package:notes_mobile/data/providers/api_provider.dart';
 import 'package:http/http.dart' as http;
 
 class AuthProvider extends ApiProvider {
   Future<http.Response> signIn(
       {required String email, required String password}) async {
-    return await http.post(uri('/users/sign_in'), body: {
+    final encodedBody = jsonEncode({
       'user': {'email': email, 'password': password}
     });
+    return await http.post(uri('/users/sign_in'),
+        headers: defaultHeaders, body: encodedBody);
   }
 }
