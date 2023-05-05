5b4bae7ad193b90c2f8e21b3eaabb84ab400c09c
rafeira <rafael12wq@gmail.com>
2023-05-05 20:39:15 -0300

    Override create action

--- notes_api/app/controllers/api/v1/users/sessions_controller.rb
+++ notes_api/app/controllers/api/v1/users/sessions_controller.rb
@@ -4,6 +4,10 @@ module Api
       class SessionsController < Devise::SessionsController
         respond_to :json
 
+        def create
+          super
+        end
+
         private
     
         def respond_with(_resource, _opts = {})
