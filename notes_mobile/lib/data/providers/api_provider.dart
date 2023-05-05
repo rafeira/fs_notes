296ff3c068b72fc8605240b59122b6c6afb449ac
rafeira <rafael12wq@gmail.com>
2023-05-05 20:39:43 -0300

    Add defaultHeaders to ApiProvider

--- notes_mobile/lib/data/providers/api_provider.dart
+++ notes_mobile/lib/data/providers/api_provider.dart
@@ -3,5 +3,9 @@ import 'package:notes_mobile/constants/apis/api_urls.dart';
 abstract class ApiProvider {
   final domain = ApiUrls.developmentUrl;
   final version = ApiUrls.v1Version;
+  final defaultHeaders = {
+    'Accept': 'application/vnd.api+json',
+    'Content-Type': 'application/json',
+  };
   Uri uri([String route = '']) => Uri.http(domain, version + route);
 }
