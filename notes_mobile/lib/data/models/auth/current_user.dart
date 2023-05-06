2cef85c9cbe1d26fc20f217866f5c3629fe207ca
rafeira <rafael12wq@gmail.com>
2023-05-06 10:20:28 -0300

    Create current user model

---
+++ notes_mobile/lib/data/models/auth/current_user.dart
@@ -0,0 +1,22 @@
+import 'package:notes_mobile/data/models/auth/response_token.dart';
+import 'package:notes_mobile/data/models/user.dart';
+
+class CurrentUser {
+  User user;
+  final ResponseToken? token;
+
+  CurrentUser({required this.user, this.token});
+  bool isLogged() => token != null;
+
+  static CurrentUser? fromJson(Map<String, dynamic>? json) {
+    if (json == null) return null;
+    final userJson = json['user'];
+    if (userJson == null) throw 'User must not be null';
+
+    return CurrentUser(user: userJson, token: json['token']);
+  }
+
+  Map<String, dynamic> toJson(CurrentUser currentUser) {
+    return {'user': user.toJson(), 'token': token?.toJson()};
+  }
+}
