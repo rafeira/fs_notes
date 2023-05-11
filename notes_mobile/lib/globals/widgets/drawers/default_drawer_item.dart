e9df1077f1f1c7f7d38bacb11349b13845c9bde8
rafeira <rafael12wq@gmail.com>
2023-05-10 22:22:39 -0300

    Wrap with InkWell widget and add onTap and titleColor fields

--- notes_mobile/lib/globals/widgets/drawers/default_drawer_item.dart
+++ notes_mobile/lib/globals/widgets/drawers/default_drawer_item.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 
 class DefaultDrawerItem extends StatelessWidget {
-  const DefaultDrawerItem({super.key, this.title});
+  const DefaultDrawerItem({super.key, this.title, this.onTap, this.titleColor});
   final String? title;
+  final GestureTapCallback? onTap;
+  final Color? titleColor;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10.0),
-      child: Text(title ?? 'Clique'),
+    return Material(
+      color: Colors.grey,
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(vertical: 10.0),
+          child: Text(
+            title ?? 'Clique',
+            style: TextStyle(color: titleColor),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
     );
   }
 }
