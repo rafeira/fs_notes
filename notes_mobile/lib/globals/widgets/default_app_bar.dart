c099825a10af596f0154fb339486d45f8f9e8db6
rafeira <rafael12wq@gmail.com>
2023-05-10 22:11:34 -0300

    Add leading field

--- notes_mobile/lib/globals/widgets/default_app_bar.dart
+++ notes_mobile/lib/globals/widgets/default_app_bar.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 
 class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
   const DefaultAppBar(
-      {super.key, this.backgroundColor, this.elevation, this.actions});
+      {super.key,
+      this.backgroundColor,
+      this.elevation,
+      this.actions,
+      this.leading});
   final Color? backgroundColor;
   final double? elevation;
   final List<Widget>? actions;
+  final Widget? leading;
   @override
   Size get preferredSize => const Size.fromHeight(40);
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      leading: leading,
       backgroundColor: backgroundColor ?? Colors.transparent,
       elevation: elevation ?? 0,
       actions: actions,
