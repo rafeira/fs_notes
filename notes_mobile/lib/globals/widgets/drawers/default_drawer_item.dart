0743ce51759aa9f714528efd5123dbac82163a81
rafeira <rafael12wq@gmail.com>
2023-05-11 21:39:36 -0300

    Add buttonColor and splashColor fields

--- notes_mobile/lib/globals/widgets/drawers/default_drawer_item.dart
+++ notes_mobile/lib/globals/widgets/drawers/default_drawer_item.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 
 class DefaultDrawerItem extends StatelessWidget {
-  const DefaultDrawerItem({super.key, this.title, this.onTap, this.titleColor});
+  const DefaultDrawerItem(
+      {super.key,
+      this.title,
+      this.onTap,
+      this.titleColor,
+      this.splashColor,
+      this.buttonColor});
   final String? title;
   final GestureTapCallback? onTap;
   final Color? titleColor;
+  final Color? splashColor;
+  final Color? buttonColor;
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.grey,
+      color: buttonColor ?? Colors.transparent,
       child: InkWell(
         onTap: onTap,
+        splashColor: splashColor,
         child: Container(
           width: double.infinity,
           padding: const EdgeInsets.symmetric(vertical: 10.0),
