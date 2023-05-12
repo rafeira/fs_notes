f09c0b8ec412836930a2d7f7faaaa44e9d1e84bb
rafeira <rafael12wq@gmail.com>
2023-05-11 21:40:32 -0300

    Enable to logout and set tittle and button colors

--- notes_mobile/lib/pages/note_list/local_widgets/drawer/note_list_menu_drawer.dart
+++ notes_mobile/lib/pages/note_list/local_widgets/drawer/note_list_menu_drawer.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:notes_mobile/globals/widgets/drawers/default_drawer.dart';
 import 'package:notes_mobile/globals/widgets/drawers/default_drawer_item.dart';
+import 'package:notes_mobile/pages/note_list/controllers/note_list_menu_drawer_controller.dart';
 
 class NoteListMenuDrawer extends StatelessWidget {
-  const NoteListMenuDrawer({super.key});
-
+  NoteListMenuDrawer({super.key});
+  final controller = NoteListMenuDrawerController();
   @override
   Widget build(BuildContext context) {
     return DefaultDrawer(
       list: [
         DefaultDrawerItem(
           title: 'Sair',
-          titleColor: Colors.red,
-          onTap: () => false,
+          titleColor: const Color.fromARGB(255, 48, 48, 48),
+          buttonColor: const Color.fromARGB(255, 242, 170, 165),
+          splashColor: Colors.red,
+          onTap: controller.logout,
         )
       ],
     );
