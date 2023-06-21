683cad513536a45a969275b1d798a4d5e72a84fb
rafeira <rafael12wq@gmail.com>
2023-06-21 18:45:38 -0300

    Do not show Authorization header if token is null

--- notes_mobile/lib/data/providers/api_provider.dart
+++ notes_mobile/lib/data/providers/api_provider.dart
@@ -4,14 +4,18 @@ import 'package:notes_mobile/data/secure/repositories/local_auth_repository.dart
 abstract class ApiProvider {
   final _localAuthRepository = LocalAuthRepository();
 
-  Future<String> get token async =>
-      (await _localAuthRepository.currentUser)!.token!.value;
+  Future<String?> get token async =>
+      (await _localAuthRepository.currentUser)?.token?.value;
   final domain = ApiUrls.developmentUrl;
   final version = ApiUrls.v1Version;
-  Future<Map<String, String>> get defaultHeaders async => {
-        'Accept': 'application/vnd.api+json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${await token}',
-      };
+  Future<Map<String, String>> get defaultHeaders async {
+    final tokenValue = await token;
+    return {
+      'Accept': 'application/vnd.api+json',
+      'Content-Type': 'application/json',
+      if (tokenValue != null) 'Authorization': 'Bearer ${await token}',
+    };
+  }
+
   Uri uri([String route = '']) => Uri.http(domain, version + route);
 }
