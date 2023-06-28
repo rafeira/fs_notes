4357854004892ae655c99b8c71f3a8047f838cad
rafeira <rafael12wq@gmail.com>
2023-06-28 20:08:14 -0300

    Create save_all class method

--- notes_api/app/models/note.rb
+++ notes_api/app/models/note.rb
@@ -1,3 +1,12 @@
 class Note < ApplicationRecord
   validates :title, :content, presence: true
+
+  def self.save_all(notes_params)
+    transaction do
+      notes_params.each do |params|
+        Note.create!(params)
+      end
+    end
+    true
+  end
 end
