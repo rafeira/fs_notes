bb2c5379bc98874e707b91dd95aec8c62274e81e
rafeira <rafael12wq@gmail.com>
2023-06-28 23:40:53 -0300

    Skip set_user for create action

--- notes_api/app/controllers/api/v1/users/sessions_controller.rb
+++ notes_api/app/controllers/api/v1/users/sessions_controller.rb
@@ -3,27 +3,28 @@ module Api
     module Users
       class SessionsController < Devise::SessionsController
         respond_to :json
+        skip_before_action :set_user, only: %i(create)
 
         def create
           super
         end
 
         private
-    
+
         def respond_with(_resource, _opts = {})
           render json: { message: 'Login efetuado com sucesso!', user: _resource }, status: :ok
         end
-    
+
         def respond_to_on_destroy
           log_out_success && return if current_user
-    
+
           log_out_failure
         end
-    
+
         def log_out_success
           render json: { message: 'Saiu com sucesso.' }, status: :ok
         end
-    
+
         def log_out_failure
           render json: { message: 'Hmm nada aconteceu.' }, status: :unauthorized
         end
